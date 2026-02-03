aadd616142b59485fd55771f4ddf3b9433eaf549
shimmerVoltage <shimmervoltage@yandex.ru>
2026-02-03 19:40:36 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,49 @@
+
+import kotlinx.coroutines.*
+
+val scope = CoroutineScope(Dispatchers.Default)
+suspend fun main() = coroutineScope {
+    //launch { doWork() }
+    //println("symbols")
+
+    /*
+    val a = async { loadA() }
+    val b = async { loadB() }
+    val c = a.await() + b.await()
+
+     */
+
+    /*
+    val job = scope.launch {
+        repeat(10){
+            delay(1000L)
+            println(it)
+        }
+    }
+    job.cancel()
+
+     */
+
+    launch (Dispatchers.IO){
+        println("start ${Thread.currentThread().name}")
+    }
+    Dispatchers.Unconfined
+    println("end ${Thread.currentThread().name}")
+}
+
+suspend fun loadA(): Int {
+    delay(1000L)
+    return 10
+}
+
+suspend fun loadB(): Int {
+    delay(1000L)
+    return 20
+}
+
+suspend fun doWork() {
+    for (i in 0..5) {
+        delay(400L)
+        println(i)
+    }
+}
